82da105a87726a76aa827ed7e4e17dcba930f865
Lyncon <lynconlyn@gmail.com>
2022-03-30 21:45:40 -0300

    Adding diagrams

--- src/main/java/com/dev/foodtravel/repositories/FoodRepository.java
+++ src/main/java/com/dev/foodtravel/repositories/FoodRepository.java
@@ -2,7 +2,6 @@ package com.dev.foodtravel.repositories;
 
 import com.dev.foodtravel.entities.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
